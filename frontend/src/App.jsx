62d0cb40df646e0683326057a5095c3a4736ccce
RohitN002 <rohitrandy002@gmail.com>
2024-04-20 13:08:51 +0530

    Day4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Day1 from './day1/Day1'
 import Day2 from './day2/Day2'
 import Day3 from './day3/Day3'
+import Day4 from './Day4/Day4'
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
     <Day1/>
     <Day2/>
     <Day3/>
-    
+    <Day4/>
     </>
   )
 }
--- frontend/src/Day4/Day4.jsx
+++ frontend/src/Day4/Day4.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Day4 = () => {
-  return (
-    <div>Day4</div>
+    const [principle,setPrinciple]=useState(1230)
+    const [intrest,setIntrest]=useState(10)
+ const [tenue,setTenue] =useState(4)
+
+ const calculateSimpleIntrest =()=>{
+    return (principle*intrest*tenue)/100
+ }
+ const handlePrincipleChange=(event)=>{
+    setPrinciple(parseFloat(event.target.value) )
+ }
+
+ const handleIntrestChange=(event)=>{
+    setIntrest(parseFloat(event.target.value) )
+ }
+ const handleTenueChange=(event)=>{
+    setTenue(parseFloat(event.target.value) )
+ }
+
+ const finalIntrest = calculateSimpleIntrest()
+ const totalAmount = calculateSimpleIntrest()+principle
+    return (
+    <div>
+        <h1>Day4</h1>
+       <div>
+<div>
+    <label htmlFor="principle">principle</label>
+    <input type="number"
+    value={principle}
+    onChange={handlePrincipleChange}
+    placeholder='enter principle amount'
+    />
+</div>
+<div>
+    <label htmlFor="intrest">intrest</label>
+    <input type="number"
+    value={intrest} 
+    onChange={handleIntrestChange}
+    placeholder='write intrest ratre'
+    />
+</div>
+<div>
+    <label htmlFor="tenue">tenue</label>
+    <input type="number"
+    value={tenue}
+    onChange={handleTenueChange} />
+
+</div>
+<div>
+    <label htmlFor="finalIntrest">Intrest Amount </label>
+    <div>{finalIntrest} </div>
+</div>
+<div>
+    <label htmlFor="totalAmount">Total Amont</label>
+    <div>{totalAmount} </div>
+</div>
+       </div>
+       
+    </div>
   )
 }
 
